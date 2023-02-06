9a12c8104b8b2733e69467be22ccd79c3f934523
joesobo <soboleskijoe@gmail.com>
2023-02-06 11:16:26 -0800

    remove REST API

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,6 @@ import cors from 'cors'
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 
-import { router as todoRouter } from './routes/todos'
 import { schema } from './schema/todoSchema'
 
 const app: express.Application = express()
@@ -11,8 +10,6 @@ const port = process.env.PORT || 4000
 app.use(cors())
 
 app.use(express.json())
-
-app.use('/todos', todoRouter)
 
 app.use(
 	'/graphql',
